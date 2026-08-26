0d0e0433ee425dac4fa4853daabec6de972fbc49
Daniel A <daniel.aldemir79@gmail.com>
2026-08-26 17:31:02 +0200

    Åtgärda fynd 1 osäker XML-hantering

--- SakerLabb.Web/Services/ImportService.cs
+++ SakerLabb.Web/Services/ImportService.cs
@@ -19,11 +19,15 @@ public class ImportService
     {
         var settings = new XmlReaderSettings
         {
-            DtdProcessing = DtdProcessing.Parse,
-            XmlResolver = new XmlUrlResolver()
+            // DTD är extra regler i XML som bland annat kan peka på filer eller andra adresser.
+            // Förbjud DTD så att en användare inte kan få servern att läsa sådant den inte borde.
+            DtdProcessing = DtdProcessing.Prohibit,
+            // Tillåt inte XML-läsaren att hämta filer eller adresser utanför dokumentet.
+            XmlResolver = null
         };
 
-        var document = new XmlDocument { XmlResolver = new XmlUrlResolver() };
+        // Dokumentet får inte heller slå upp externa resurser när XML-innehållet läses in.
+        var document = new XmlDocument { XmlResolver = null };
         using var reader = XmlReader.Create(new StringReader(xml), settings);
         document.Load(reader);
 
